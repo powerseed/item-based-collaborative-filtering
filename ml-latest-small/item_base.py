26ef24bf2ada2d74a0bba7cdc2c337bd5618319e
YidongWei <weiy3458@myumanitoba.ca>
2019-10-11 21:09:06 -0500

    partially update comments

--- ml-latest-small/item_base.py
+++ ml-latest-small/item_base.py
@@ -14,28 +14,28 @@ train_pivot = train_csv.pivot(index = 'movieId', columns = 'userId', values = 'a
 train_mat = np.asarray(train_pivot)
 train_mat = train_mat[:,1:]
 i = 0
-sum_residual = 0
-prediction = []
-mean_mat = {}
-for index,row in Means.iterrows():
-    mean_mat[row['movieId']]=row['rating_mean']
-for index,row in test_csv.iterrows():
-    if i % 10 == 0:
-        print(i)
+sum_residual = 0##the sum of the residual over all the prediction
+prediction = []## storing the prediction, basically necessary, but for me to debugging use
+mean_mat = {}## a python dictionary, contain key:value, key will be movieId and value will be mean_rating
+for index,row in Means.iterrows():##each row in the mean data frame
+    mean_mat[row['movieId']]=row['rating_mean'] ## we retrieve the movieId and the mean_rating to the dictionary
+for index,row in test_csv.iterrows():##each row in the data we need to predict
+    if i % 10 == 0:##just showing me the progress of my program
+        print("{} out of {}".format(i, test_csv.shape[0]))
     i = i + 1
     ##the user we are predicting
     predic_user_index = int(row['userId'])-1
     ##the movie we are predicting
     predic_movie_index = int(row['movieId'])-1
-    similarity = []
-    rated_index = []
-    user_rated = []
+    similarity = []## contain the similarity of the predicting movie to the movie the user already watch
+    rated_index = []## cotain the index of the movie
+    user_rated = []## contain the rating of user
     for movie_id in range(train_mat.shape[0]):
-        if train_mat[movie_id,predic_user_index] != 0:
+        if train_mat[movie_id,predic_user_index] != 0: ## if this movie the user watch before
             rated_index.append(movie_id)
-            user_rated.append(train_mat[movie_id,predic_user_index])
-            rated1 = []
-            rated2 = []
+            user_rated.append(train_mat[movie_id,predic_user_index])##append the rating
+            rated1 = []##the rating for the 
+            rated2 = []## the rating for the predict movie
             for user_id in range(train_mat.shape[1]):
                 if train_mat[movie_id,user_id] != 0 and train_mat[predic_movie_index,user_id] != 0:
                     rated1.append(train_mat[movie_id,user_id])

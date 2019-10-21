86d2ff4a8dd2a448e861928784a2a36d64f9a965
YidongWei <weiy3458@myumanitoba.ca>
2019-10-20 19:36:35 -0500

    modified

--- matrix_factorization/matrix_f.py
+++ matrix_factorization/matrix_f.py
@@ -4,7 +4,7 @@ import numpy as np
 import mflibrary as mf
 import math
 #r_csv = pd.read_csv('../data/training_ratings.csv')[['userId','movieId','rating','timestamp']].rename(columns={'movieId':'movieId1'})
-m_csv = pd.read_csv('../data/movies.csv', )[['movieId']]
+#m_csv = pd.read_csv('../data/movies.csv', )[['movieId']]
 #r_csv = pd.merge(m_csv,r_csv, left_on = 'movieId', right_on = 'movieId1', how ='left')
 #r_csv = r_csv.fillna(0)
 #r_mat = np.asarray(r_csv.pivot(index = 'movieId', columns = 'userId', values = 'rating').fillna(0))[:,1:]
--- matrix_factorization/mflibrary.py
+++ matrix_factorization/mflibrary.py
@@ -1,6 +1,6 @@
 import numpy as np# -*- coding: utf-8 -*-
 
-def matrix_fatorization(R,I,U,K, step=5000, alpha = 0.0002, beta = 0.02):
+def matrix_fatorization(R,I,U,K, step=5000, alpha = 0.0005, beta = 0.02):
     assert I.shape[1] == K and U.shape[0] == K
     for s in range(step+1):##add one since there is one step is for check for the origin input
         for i in range(R.shape[0]):
@@ -8,8 +8,9 @@ def matrix_fatorization(R,I,U,K, step=5000, alpha = 0.0002, beta = 0.02):
                 if R[i,j] > 0:
                     eij = R[i,j] - np.sum(I[i,:] * U[:,j])
                     for k in range(K):
-                        I[i,k] = I[i,k] + alpha*((2*eij*U[k,j]) - beta*I[i,k])
-                        U[k,j] = U[k,j] + alpha*((2*eij*I[i,k]) - beta*U[k,j])
+                        I[i,k] = I[i,k] + alpha*((2.0*eij*U[k,j]) - beta*I[i,k])
+                        U[k,j] = U[k,j] + alpha*((2.0*eij*I[i,k]) - beta*U[k,j])
+                        
                     
         e = 0
         for i in range(R.shape[0]):
@@ -18,6 +19,6 @@ def matrix_fatorization(R,I,U,K, step=5000, alpha = 0.0002, beta = 0.02):
                     e = e + pow(R[i,j] - np.sum(I[i,:] * U[:,j]),2)
                     e = e + beta/2*(np.sum(np.power(I[i,:],2)) + np.sum(np.power(U[:,j],2)))
         print("error: {}, step: {}".format(e,s))
-        if e < 0.5:
+        if e < 500000:
             break
     return I,U

41bbdfcede69931d05aee64e78821cac75cbab4a
Jiehao Luo <powerseed5044@gmail.com>
2019-11-16 00:00:06 -0600

    clustering

--- fuzzyRoughSetCode/calculate_pos_neg.py
+++ fuzzyRoughSetCode/calculate_pos_neg.py
@@ -4,6 +4,16 @@ filename = 'weather_features.csv'
 decision_col = 'weather_main'
 col_to_drop = 'dt_iso'
 
+# parameter:
+#   filename: the name of the database csv file.
+#   decision_col: the name of the decision attribute
+#   col_to_drop: column that cannot be processed when doing the reduct, like "date".
+#
+# return:
+#   col_name_reduct: the names of remaining columns after the reduct
+#   U_X: elementary sets based on the decision attribute
+#   U_R: elementary sets based on all conditional attributes
 col_name_reduct, U_X, U_R = calculate_UX_UR.calculate_UX_UR(filename, decision_col, col_to_drop)
+
 # print(col_name_reduct)
 # print(U_R)

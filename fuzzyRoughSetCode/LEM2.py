f976f650d7693eb98d43c9a2e1480012f51081ae
Jiehao Luo <powerseed5044@gmail.com>
2019-11-20 16:20:19 -0600

    clustering

--- fuzzyRoughSetCode/LEM2.py
+++ fuzzyRoughSetCode/LEM2.py
@@ -6,6 +6,10 @@ from fuzzyRoughSetCode.calculate_UX_UR import calculate_UX_UR
 from fuzzyRoughSetCode.calculate_pos_neg import calculate_upper_lower
 import pandas as pd
 
+# filename = 'breast_cancer.csv'
+# decision_col = 'diagnosis'
+# col_to_drop = 'id'
+
 filename = 'breast_cancer.csv'
 decision_col = 'diagnosis'
 col_to_drop = 'id'
@@ -14,13 +18,16 @@ table = pd.read_csv('../data/' + filename)
 table = table.drop(columns=[col_to_drop])
 
 col_name_reduct, U_X, U_R = calculate_UX_UR(table, decision_col)
-all_upper, all_lower, allaR, allBnd = calculate_upper_lower(U_X, U_R)
-
-print("col_name_reduct: ", col_name_reduct)
-print("U_X: ", U_X)
-print("U_R: ", U_R)
-print("all_lower: ", all_lower)
-
+allupper_rules, alllower_rules, allupper_records, alllower_records, allaR, allBnd = calculate_upper_lower(U_X, U_R)
+#
+# qweqwewq = copy.deepcopy(alllower_records)
+# qweqwewq = qweqwewq["M"]
+# qweqwewq = list(qweqwewq)
+# qweqwewq.sort()
+# print("U_X: ", U_X)
+# print("col_name_reduct: ", col_name_reduct)
+# print("alllower_rules: ", alllower_rules)
+# print("alllower_records: ", qweqwewq)
 
 def get_all_t():
     all_t = {}
@@ -59,10 +66,16 @@ def calculate_all_relevant_T_G(G):
 
 
 def get_all_item_in_T(T):
+    all_set_t = []
+    for t in T:
+        all_set_t.append(all_t[t[0]][t[1]])
+
     all_items_in_T = set()
 
-    for t in T:
-        all_items_in_T = all_items_in_T.union(all_t[t[0]][t[1]])
+    if len(all_set_t) != 0:
+        all_items_in_T = all_set_t[0]
+        for t in all_set_t:
+            all_items_in_T = all_items_in_T.intersection(t)
 
     return all_items_in_T
 
@@ -113,14 +126,61 @@ def delete_T_from_hollow_T(hollow_T, T_to_delete):
     copy_hollow_T.remove(the_T_to_delete)
     return copy_hollow_T
 
+def select_a_t(all_relevant_T_G, G):
+    list_all_relevant_T_G = list(all_relevant_T_G)
+    list_cardinality = []
+
+    index = 0
+    while index < len(list_all_relevant_T_G):
+        t = list_all_relevant_T_G[index]
+        set_t = all_t[t[0]][t[1]]
+        t_intersect_G = set_t.intersection(G)
+        cardinality = len(t_intersect_G)
+        list_cardinality.append(cardinality)
+        index = index + 1
+
+    max_cardinality = max(list_cardinality)
+    indices_max_cardinality = []
+    index = 0
+    while index < len(list_cardinality):
+        if list_cardinality[index] == max_cardinality:
+            indices_max_cardinality.append(index)
+        index = index + 1
+
+    if len(indices_max_cardinality) == 1:
+        return list_all_relevant_T_G[indices_max_cardinality[0]]
+    else:
+        ts_with_same_cardinality = []
+        for index in indices_max_cardinality:
+            ts_with_same_cardinality.append(list_all_relevant_T_G[index])
+
+        cardinalities_t = []
+        for t in ts_with_same_cardinality:
+            cardinality_t = len(all_t[t[0]][t[1]])
+            cardinalities_t.append(cardinality_t)
+
+        min_cardinality = min(cardinalities_t)
+        indices_min_cardinality = []
+        index = 0
+        while index < len(cardinalities_t):
+            if cardinalities_t[index] == min_cardinality:
+                indices_min_cardinality.append(index)
+            index = index + 1
+
+        if len(indices_min_cardinality) == 1:
+            return ts_with_same_cardinality[indices_min_cardinality[0]]
+        else:
+            return list_all_relevant_T_G[0]
+
 
 conclusion = {}
-for decision in all_lower:
+for decision in alllower_records:
     conclusion[decision] = []
-    B = set(U_X[decision])
+    B = alllower_records[decision]
     G = copy.deepcopy(B)
     Covering_hollow_T = set()
 
+    count = 0;
     while (len(G) != 0):
         condition_T = set()
         all_relevant_T_G = calculate_all_relevant_T_G(G)  # set contains t
@@ -129,8 +189,10 @@ for decision in all_lower:
         T_belongs_to_B = get_all_item_in_T(condition_T).issubset(B)
 
         while len(all_relevant_T_G) != 0 and (len(condition_T) == 0 or (not T_belongs_to_B)):  # while T = Ø or [T] Õ/ B
-            t = all_relevant_T_G.pop()
-            all_relevant_T_G.add(t)
+            t = select_a_t(all_relevant_T_G, G)
+            all_relevant_T_G.remove(t)
+            # print("condition_T 1: ", condition_T)
+            # print("t 1: ", t)
             temp = set()
             temp.add(tuple(t))
             condition_T = condition_T.union(temp)
@@ -140,6 +202,9 @@ for decision in all_lower:
             T_belongs_to_B = get_all_item_in_T(condition_T).issubset(B)
         # end inner while
 
+        # if len(condition_T) == 1:
+        #     print("condition_T: ", condition_T, " count: ", count)
+        #     break
         # for each t in T do
         set_t_to_delete_from_condition_T = []
         for t in condition_T:
@@ -150,19 +215,19 @@ for decision in all_lower:
             delete_t_from_T(copy_T, t)
             # print("copy 2: ", copy_T)
 
-            if copy_T != None:
+            if copy_T != None and len(copy_T) != 0:
                 all_items_in_remove_t = get_all_item_in_T(copy_T)
                 # print("copy_T: ", copy_T)
                 # print("all_items_in_remove_t: ", all_items_in_remove_t)
                 # print("B: ", B)
                 if all_items_in_remove_t.issubset(B):
-                    print("true")
+                    # print("true")
                     set_t_to_delete_from_condition_T.append(t)
 
         if len(set_t_to_delete_from_condition_T) != 0:
-            print("set_t_to_delete_from_condition_T: ", set_t_to_delete_from_condition_T)
+            # print("set_t_to_delete_from_condition_T: ", set_t_to_delete_from_condition_T)
             list_condition_T = list(condition_T)
-            print("list_condition_T: before", list_condition_T)
+            # print("list_condition_T: before", list_condition_T)
             for t in set_t_to_delete_from_condition_T:
                 index = 0
                 while index < len(list_condition_T):
@@ -171,7 +236,7 @@ for decision in all_lower:
                     else:
                         index = index + 1
             condition_T = set(list_condition_T)
-            print("condition_T: after", condition_T)
+            # print("condition_T: after", condition_T)
         # end: for each t in T do
 
         # print("Covering_hollow_T, before union: ", Covering_hollow_T)
@@ -185,6 +250,7 @@ for decision in all_lower:
         # print("get_all_item_in_hollow_T(Covering_hollow_T): ", get_all_item_in_hollow_T(Covering_hollow_T))
         G = B.difference(get_all_item_in_hollow_T(Covering_hollow_T))
         # print("G1: after", G)
+        count = count + 1
     # outer while
     T_to_delete = []
     for T in Covering_hollow_T:
@@ -199,16 +265,16 @@ for decision in all_lower:
         list_B = list(B)
         list_B.sort()
 
-        print("list_all_item_in_hollowT_minus_T: ", list_all_item_in_hollowT_minus_T)
-        print("list_B: ", list_B)
+        # print("list_all_item_in_hollowT_minus_T: ", list_all_item_in_hollowT_minus_T)
+        # print("list_B: ", list_B)
         if list_all_item_in_hollowT_minus_T == list_B:
             print("true 2")
             # print("Covering_hollow_T 1: ", Covering_hollow_T)
             T_to_delete.append(T)
             # print("Covering_hollow_T 2: ", Covering_hollow_T)
 
-    print("T_to_delete: ", T_to_delete)
-    print("Covering_hollow_T: before", Covering_hollow_T)
+    # print("T_to_delete: ", T_to_delete)
+    # print("Covering_hollow_T: before", Covering_hollow_T)
     for T in T_to_delete:
         Covering_hollow_T.remove(T)
 
--- fuzzyRoughSetCode/calculate_pos_neg.py
+++ fuzzyRoughSetCode/calculate_pos_neg.py
@@ -1,28 +1,39 @@
 def calculate_upper_lower(U_X, U_R):
-    allupper = {}
-    alllower = {}
+    allupper_rules = {}
+    allupper_records = {}
+
+    alllower_rules = {}
+    alllower_records = {}
+
     allaR = []
     allBnd = []
 
     for decision in U_X:
-        upperap = []
         lowerap = []
-        U_Xcon = set()
-        for item1 in U_X[decision]:
-            U_Xcon.add(item1)
+        upperap = []
+
+        lowerap_records = set()
+        upperap_records = set()
+
+        U_Xcon = set(U_X[decision])
         for result in U_R:
-            U_Rcon = set()
+            U_Rcon = set(U_R[result])
             # if decision is a subset of result
-            for item in U_R[result]:
-                U_Rcon.add(item)
             if (U_Rcon.issubset(U_Xcon)):
                 lowerap.append(result)
+                lowerap_records = lowerap_records.union(U_Rcon)
             # if decision contains element of result
             if (U_Rcon.isdisjoint(U_Xcon) == False):
                 upperap.append(result)
-        allupper[decision] = upperap
-        alllower[decision] = lowerap
+                upperap_records = upperap_records.union(U_Rcon)
+
+        alllower_rules[decision] = lowerap
+        alllower_records[decision] = lowerap_records
+
+        allupper_rules[decision] = upperap
+        allupper_records[decision] = upperap_records
+
         # allaR.add(len(lowerap)/len(upperap)) #粗糙度
     # allBnd.append(lowerap - upperap)#bond
 
-    return allupper, alllower, allaR, allBnd
+    return allupper_rules, alllower_rules, allupper_records, alllower_records, allaR, allBnd

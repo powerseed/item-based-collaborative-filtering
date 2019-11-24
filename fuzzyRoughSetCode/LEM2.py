fd6e5f73f607dc46009bd57228d8bfe9be78c23d
Jiehao Luo <powerseed5044@gmail.com>
2019-11-24 10:45:47 -0600

    upper bound LEM2 rules

--- fuzzyRoughSetCode/LEM2.py
+++ fuzzyRoughSetCode/LEM2.py
@@ -49,7 +49,6 @@ def populate_all_t():
 
 
 populate_all_t()
-print("all_t: ", all_t)
 
 
 def calculate_all_relevant_T_G(G):
@@ -172,116 +171,125 @@ def select_a_t(all_relevant_T_G, G):
         else:
             return list_all_relevant_T_G[0]
 
+def lem2(lower_or_upper_set):
+    conclusion = {}
+    for decision in lower_or_upper_set:
+        conclusion[decision] = []
+        B = alllower_records[decision]
+        G = copy.deepcopy(B)
+        Covering_hollow_T = set()
 
-conclusion = {}
-for decision in alllower_records:
-    conclusion[decision] = []
-    B = alllower_records[decision]
-    G = copy.deepcopy(B)
-    Covering_hollow_T = set()
+        count = 0
+        while (len(G) != 0):
+            condition_T = set()
+            all_relevant_T_G = calculate_all_relevant_T_G(G)  # set contains t
+            # print("all_relevant_T_G: ", all_relevant_T_G)
 
-    count = 0;
-    while (len(G) != 0):
-        condition_T = set()
-        all_relevant_T_G = calculate_all_relevant_T_G(G)  # set contains t
-        # print("all_relevant_T_G: ", all_relevant_T_G)
-
-        T_belongs_to_B = get_all_item_in_T(condition_T).issubset(B)
-
-        while len(all_relevant_T_G) != 0 and (len(condition_T) == 0 or (not T_belongs_to_B)):  # while T = Ø or [T] Õ/ B
-            t = select_a_t(all_relevant_T_G, G)
-            all_relevant_T_G.remove(t)
-            # print("condition_T 1: ", condition_T)
-            # print("t 1: ", t)
-            temp = set()
-            temp.add(tuple(t))
-            condition_T = condition_T.union(temp)
-            G = G.intersection(all_t[t[0]][t[1]])  # G := [t] « G;
-            all_relevant_T_G = calculate_all_relevant_T_G(G)
-            all_relevant_T_G = all_relevant_T_G.difference(condition_T)  # T(G) := T(G) – T;
             T_belongs_to_B = get_all_item_in_T(condition_T).issubset(B)
-        # end inner while
 
-        # if len(condition_T) == 1:
-        #     print("condition_T: ", condition_T, " count: ", count)
-        #     break
-        # for each t in T do
-        set_t_to_delete_from_condition_T = []
-        for t in condition_T:
-            # print("condition_T 1: ", condition_T)
-            copy_T = copy.deepcopy(condition_T)
-            # print("copy 1: ", copy_T)
-            # print("t 1: ", t)
-            delete_t_from_T(copy_T, t)
-            # print("copy 2: ", copy_T)
+            while len(all_relevant_T_G) != 0 and (len(condition_T) == 0 or (not T_belongs_to_B)):  # while T = Ø or [T] Õ/ B
+                t = select_a_t(all_relevant_T_G, G)
+                all_relevant_T_G.remove(t)
+                # print("condition_T 1: ", condition_T)
+                # print("t 1: ", t)
+                temp = set()
+                temp.add(tuple(t))
+                condition_T = condition_T.union(temp)
+                G = G.intersection(all_t[t[0]][t[1]])  # G := [t] « G;
+                all_relevant_T_G = calculate_all_relevant_T_G(G)
+                all_relevant_T_G = all_relevant_T_G.difference(condition_T)  # T(G) := T(G) – T;
+                T_belongs_to_B = get_all_item_in_T(condition_T).issubset(B)
+            # end inner while
 
-            if copy_T != None and len(copy_T) != 0:
-                all_items_in_remove_t = get_all_item_in_T(copy_T)
-                # print("copy_T: ", copy_T)
-                # print("all_items_in_remove_t: ", all_items_in_remove_t)
-                # print("B: ", B)
-                if all_items_in_remove_t.issubset(B):
-                    # print("true")
-                    set_t_to_delete_from_condition_T.append(t)
+            # if len(condition_T) == 1:
+            #     print("condition_T: ", condition_T, " count: ", count)
+            #     break
+            # for each t in T do
+            set_t_to_delete_from_condition_T = []
+            for t in condition_T:
+                # print("condition_T 1: ", condition_T)
+                copy_T = copy.deepcopy(condition_T)
+                # print("copy 1: ", copy_T)
+                # print("t 1: ", t)
+                delete_t_from_T(copy_T, t)
+                # print("copy 2: ", copy_T)
 
-        if len(set_t_to_delete_from_condition_T) != 0:
-            # print("set_t_to_delete_from_condition_T: ", set_t_to_delete_from_condition_T)
-            list_condition_T = list(condition_T)
-            # print("list_condition_T: before", list_condition_T)
-            for t in set_t_to_delete_from_condition_T:
-                index = 0
-                while index < len(list_condition_T):
-                    if list_condition_T[index][0] == t[0] and list_condition_T[index][1] == t[1]:
-                        list_condition_T.pop(index)
-                    else:
-                        index = index + 1
-            condition_T = set(list_condition_T)
-            # print("condition_T: after", condition_T)
-        # end: for each t in T do
+                if copy_T != None and len(copy_T) != 0:
+                    all_items_in_remove_t = get_all_item_in_T(copy_T)
+                    # print("copy_T: ", copy_T)
+                    # print("all_items_in_remove_t: ", all_items_in_remove_t)
+                    # print("B: ", B)
+                    if all_items_in_remove_t.issubset(B):
+                        # print("true")
+                        set_t_to_delete_from_condition_T.append(t)
 
-        # print("Covering_hollow_T, before union: ", Covering_hollow_T)
-        # print("condition_T: ", condition_T)
-        temp = set()
-        temp.add(tuple(condition_T))
-        Covering_hollow_T = Covering_hollow_T.union(temp)
-        # print("Covering_hollow_T, after union: ", Covering_hollow_T)
+            if len(set_t_to_delete_from_condition_T) != 0:
+                # print("set_t_to_delete_from_condition_T: ", set_t_to_delete_from_condition_T)
+                list_condition_T = list(condition_T)
+                # print("list_condition_T: before", list_condition_T)
+                for t in set_t_to_delete_from_condition_T:
+                    index = 0
+                    while index < len(list_condition_T):
+                        if list_condition_T[index][0] == t[0] and list_condition_T[index][1] == t[1]:
+                            list_condition_T.pop(index)
+                        else:
+                            index = index + 1
+                condition_T = set(list_condition_T)
+                # print("condition_T: after", condition_T)
+            # end: for each t in T do
 
-        # print("B1: ", B)
-        # print("get_all_item_in_hollow_T(Covering_hollow_T): ", get_all_item_in_hollow_T(Covering_hollow_T))
-        G = B.difference(get_all_item_in_hollow_T(Covering_hollow_T))
-        # print("G1: after", G)
-        count = count + 1
-    # outer while
-    T_to_delete = []
-    for T in Covering_hollow_T:
-        # print("Covering_hollow_T: ", Covering_hollow_T)
-        # print("T: ", T)
-        hollowT_minus_T = delete_T_from_hollow_T(Covering_hollow_T, T)
-        # print("hollowT_minus_T: ", hollowT_minus_T)
+            # print("Covering_hollow_T, before union: ", Covering_hollow_T)
+            # print("condition_T: ", condition_T)
+            temp = set()
+            temp.add(tuple(condition_T))
+            Covering_hollow_T = Covering_hollow_T.union(temp)
+            # print("Covering_hollow_T, after union: ", Covering_hollow_T)
 
-        all_item_in_hollowT_minus_T = get_all_item_in_hollow_T(hollowT_minus_T)
-        list_all_item_in_hollowT_minus_T = list(all_item_in_hollowT_minus_T)
-        list_all_item_in_hollowT_minus_T.sort()
-        list_B = list(B)
-        list_B.sort()
+            # print("B1: ", B)
+            # print("get_all_item_in_hollow_T(Covering_hollow_T): ", get_all_item_in_hollow_T(Covering_hollow_T))
+            G = B.difference(get_all_item_in_hollow_T(Covering_hollow_T))
+            # print("G1: after", G)
+            count = count + 1
+        # outer while
+        T_to_delete = []
+        for T in Covering_hollow_T:
+            # print("Covering_hollow_T: ", Covering_hollow_T)
+            # print("T: ", T)
+            hollowT_minus_T = delete_T_from_hollow_T(Covering_hollow_T, T)
+            # print("hollowT_minus_T: ", hollowT_minus_T)
 
-        # print("list_all_item_in_hollowT_minus_T: ", list_all_item_in_hollowT_minus_T)
-        # print("list_B: ", list_B)
-        if list_all_item_in_hollowT_minus_T == list_B:
-            print("true 2")
-            # print("Covering_hollow_T 1: ", Covering_hollow_T)
-            T_to_delete.append(T)
-            # print("Covering_hollow_T 2: ", Covering_hollow_T)
+            all_item_in_hollowT_minus_T = get_all_item_in_hollow_T(hollowT_minus_T)
+            list_all_item_in_hollowT_minus_T = list(all_item_in_hollowT_minus_T)
+            list_all_item_in_hollowT_minus_T.sort()
+            list_B = list(B)
+            list_B.sort()
 
-    # print("T_to_delete: ", T_to_delete)
-    # print("Covering_hollow_T: before", Covering_hollow_T)
-    for T in T_to_delete:
-        Covering_hollow_T.remove(T)
+            # print("list_all_item_in_hollowT_minus_T: ", list_all_item_in_hollowT_minus_T)
+            # print("list_B: ", list_B)
+            if list_all_item_in_hollowT_minus_T == list_B:
+                print("true 2")
+                # print("Covering_hollow_T 1: ", Covering_hollow_T)
+                T_to_delete.append(T)
+                # print("Covering_hollow_T 2: ", Covering_hollow_T)
 
-    conclusion[decision].append(Covering_hollow_T)
+        # print("T_to_delete: ", T_to_delete)
+        # print("Covering_hollow_T: before", Covering_hollow_T)
+        for T in T_to_delete:
+            Covering_hollow_T.remove(T)
 
-print("Conclusion: ")
-for decision in conclusion:
-    print("The conditions to achieve the decision of ", decision, " are: ")
-    for T in conclusion[decision][0]:
+        conclusion[decision].append(Covering_hollow_T)
+    return conclusion
+
+conclusion_lower = lem2(alllower_records)
+print("Conclusion for lower bound: ")
+for decision in conclusion_lower:
+    print("The conditions that are bound to cause the decision of ", decision, " are: ")
+    for T in conclusion_lower[decision][0]:
+        print(T)
+
+conclusion_upper = lem2(allupper_records)
+print("Conclusion for upper bound: ")
+for decision in conclusion_upper:
+    print("The conditions that could cause the decision of ", decision, " are: ")
+    for T in conclusion_upper[decision][0]:
         print(T)

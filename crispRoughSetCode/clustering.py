d21950b46e826dd8bfbb35baef99f708c982bd4b
YidongWei <weiy3458@myumanitoba.ca>
2019-12-05 15:56:07 -0600

    hh

--- crispRoughSetCode/clustering.py
+++ crispRoughSetCode/clustering.py
@@ -14,7 +14,7 @@ def cluster_table(table,decision_col,n_clusters,output_file_name = -1,cat = []):
 def cluster(column,n_cluster):
     kmodel = KMeans(n_clusters = n_cluster, n_jobs = 8,random_state = 10) #建立模型，n_jobs是并行数，一般等于CPU数较好
     kmodel.fit(column.values.reshape((len(column), 1))) #训练模型
-    d3 = kmodel.transform(column.values.reshape((len(column), 1)))
+    d3 = kmodel.predict(column.values.reshape((len(column), 1)))
 #    c = pd.DataFrame(kmodel.cluster_centers_).sort_values(0)  #输出聚类中心，并且排序（默认是随机序的）
 #    w = c.rolling(2).mean().iloc[1:] #相邻两项求中点，作为边界点
 #    w = [column.min()] + list(w[0]) + [column.max()] #把首末边界点加上，w[0]中0为列索引
